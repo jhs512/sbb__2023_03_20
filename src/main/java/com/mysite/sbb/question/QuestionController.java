3148ebade6ef87fbf4bce2a412e311038dc7616b
jhs512 <jangka512@gmail.com>
2023-03-28 14:59:05 +0900

    author 엔티티 추가

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,6 +1,8 @@
 package com.mysite.sbb.question;
 
 import com.mysite.sbb.answer.AnswerForm;
+import com.mysite.sbb.user.SiteUser;
+import com.mysite.sbb.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -9,15 +11,18 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
+
 @Controller
 @RequestMapping("/question")
 @RequiredArgsConstructor
 // @Validated 컨트롤러에서는 이 부분 생략가능
 public class QuestionController {
     private final QuestionService questionService;
+    private final UserService userService;
 
     @GetMapping("/list")
-        public String list(Model model, @RequestParam(defaultValue = "0") int page) {
+    public String list(Model model, @RequestParam(defaultValue = "0") int page) {
         Page<Question> paging = questionService.getList(page);
 
         model.addAttribute("paging", paging);
@@ -47,14 +52,16 @@ public class QuestionController {
     // @Valid QuestionForm questionForm
     // questionForm 값을 바인딩 할 때 유효성 체크를 해라!
     // questionForm 변수와 bindingResult 변수는 model.addAttribute 없이 바로 뷰에서 접근할 수 있다.
-    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
-        if ( bindingResult.hasErrors() ) {
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult, Principal principal) {
+        if (bindingResult.hasErrors()) {
             // question_form.html 실행
             // 다시 작성하라는 의미로 응답에 폼을 실어서 보냄
             return "question_form";
         }
 
-        questionService.create(questionForm.getSubject(), questionForm.getContent());
+        SiteUser siteUser = userService.getUser(principal.getName());
+
+        questionService.create(questionForm.getSubject(), questionForm.getContent(), siteUser);
 
         return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
     }
